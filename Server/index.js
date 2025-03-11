302a785b81847f848a4a99598617f446b4d52006
Shubh586 <shubhamsolanke9326@gmail.com>
2025-03-11 19:35:44 +0530

    Fix Vite routing with Express

--- Server/index.js
+++ Server/index.js
@@ -7,10 +7,12 @@ import AuthRoutes from "./routes/AuthRoutes.js";
 import path from "path";
 import contackRouter from "./routes/ContactRouter.js";
 import messageRouter from "./routes/MessageRoutes.js";
-
+import { fileURLToPath } from "url";
 import setupSocket from "./socket.js";
 import ChannelRouter from "./routes/ChannelRoutes.js";
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cookieParser());
 app.use(
@@ -25,11 +27,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use('/uploads/files', express.static('uploads/files'));
+
 const port = process.env.PORT || 5000;
 app.use("/api/auth", AuthRoutes);
 app.use("/api/search", contackRouter);
 app.use("/api/message", messageRouter);
 app.use('/api/channel', ChannelRouter);
+app.use(express.static(path.join(__dirname, "Client", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
 });
